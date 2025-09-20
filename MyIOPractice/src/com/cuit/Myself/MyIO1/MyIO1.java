cfb4b213032773e0dd362e3e344d2693f053914e
刘航宇 <2605172384@qq.com>
2025-09-20 15:14:49 +0800

    add .gitignore and initial project files

---
+++ MyIOPractice/src/com/cuit/Myself/MyIO1/MyIO1.java
@@ -0,0 +1,16 @@
+package com.cuit.Myself.MyIO1;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MyIO1 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("myIO.txt");
+        int byte1 = fis.read();
+        System.out.println(byte1);
+
+        char byte2 = (char)fis.read();
+        System.out.println(byte2);
+    }
+}
